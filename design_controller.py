3b3aa7656c3e0d6e6f8ef1c84d4a0ff9f102352e
miketynes <mtynes95@gmail.com>
2019-12-12 04:22:30 -0500

    doc controller

--- design_controller.py
+++ design_controller.py
@@ -1,3 +1,8 @@
+"""A controller class that executes command objects defined in commands.py on the design space
+
+Module also includes custom Exceptions related to undo history.
+"""
+
 
 class EmptyUndoStackError(Exception):
     pass
@@ -10,6 +15,12 @@ class EmptyRedoStackError(Exception):
 class DesignController:
 
     def __init__(self):
+        """Manage the history of the design space without explicitly recording states using command objects.
+
+        Design for this class was heavily influenced by code found here: https://derdon.github.io/blog/implementing-an-undo-redo-manager-in-python.html
+
+        Methods are undocumented as they are considered self-documenting and are not public-facing.
+        """
 
         self.undo_commands = []
         self.redo_commands = []
@@ -40,13 +51,11 @@ class DesignController:
         self.redo_commands[:] = []
 
     def do(self, command):
-
         command.execute()
         self.push_undo_command(command)
         self.clear_redo_commands()
 
     def undo(self, n=1):
-
         for _ in range(n):
             command = self.pop_undo_command()
             command.undo()
