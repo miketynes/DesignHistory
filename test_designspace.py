80ba2e4f1b388b7d9b9a410a98ab3f0ba97f0c43
miketynes <mtynes95@gmail.com>
2019-12-11 23:48:05 -0500

    finish testing design space

--- test_designspace.py
+++ test_designspace.py
@@ -4,10 +4,7 @@ from brick import Brick
 import designspace
 
 
-
-
 class TestDesign(unittest.TestCase):
-
 
     def setUp(self):
         self.design = designspace.DesignSpace()
@@ -21,13 +18,15 @@ class TestDesign(unittest.TestCase):
         self.rot1 = [7, 8, 9]
         self.rot2 = [10, 11, 12]
 
+    def tearDown(self):
+        self.design.reset()
+
     def test_singletonness(self):
         """Make sure that this module's design and the one built in designspace are the same
 
         i.e. make sure that the designspace.DesignSpace object is really a singleton
         """
         self.assertIs(self.design, designspace.design)
-
 
     def test_add(self):
         self.design.add(self.b1, self.pos1, self.rot1)
@@ -44,15 +43,17 @@ class TestDesign(unittest.TestCase):
             self.design.move(self.b2, self.pos1, self.rot2)
 
     def test_delete(self):
-        pass
+        self.design.add(self.b1, self.pos1, self.rot1)
+        self.design.delete(self.b1)
+        self.assertEqual(len(self.design), 0)
+        with self.assertRaises(ValueError):
+            self.design.delete(self.b1)
 
     def test_hash(self):
         """Test the design level hash function"""
 
         # first, test that it works at all
         self.assertIsInstance(hash(self.design), int)
-
-        empty_design_hash = hash(self.design)
 
 
 if __name__ == '__main__':
