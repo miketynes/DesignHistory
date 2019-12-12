b0aa0cbcad72f4663cf813a7ac6adeb210ab2d59
miketynes <mtynes95@gmail.com>
2019-12-12 00:37:10 -0500

    fix bugs in move and delete

--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import designspace
 design = designspace.DesignSpace()
 
@@ -21,13 +23,18 @@ class MoveBrickInDesign:
     def __init__(self, brick, position, orientation):
 
         self.brick = brick
-        self.from_pos = brick.position
-        self.from_rot = brick.orientation
+
+        # from locations are set at execute time
+        self.from_pos = None
+        self.from_rot = None
 
         self.to_pos = position
         self.to_rot = orientation
 
     def execute(self):
+        # save old positions immediately before moving
+        self.from_pos = deepcopy(self.brick.position)
+        self.from_rot = deepcopy(self.brick.orientation)
         design.move(self.brick, self.to_pos, self.to_rot)
 
     def undo(self):
@@ -42,4 +49,7 @@ class DeleteBrickFromDesign:
         self.from_rot = brick.orientation
 
     def execute(self):
+        design.delete(self.brick)
+
+    def undo(self):
         design.add(self.brick, self.from_pos, self.from_rot)
